9b96cbe850aeeba2e67ba298fbe3f6970536c874
Lmigua <1768089277@qq.com>
2025-12-03 17:08:54 +0800

    锁定竖屏，跟换直播页引用地址

--- pages/index/live.vue
+++ pages/index/live.vue
@@ -1,10 +1,11 @@
 <template>
     <view>
-        <web-view src="https://lotto.sina.cn/video/tcopen/"></web-view>
+        <web-view src="https://appstatic.sporttery.cn/dl/h5/live/index/index.html"></web-view>
     </view>
 </template>
 <script>
 // https://appstatic.sporttery.cn/dl/h5/live/index/index.html
+// https://lotto.sina.cn/video/tcopen/
 export default {
 
 }
