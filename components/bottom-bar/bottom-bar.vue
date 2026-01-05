92ba3a43af4b63e06699028480c19db486c7ed83
Lmigua <1768089277@qq.com>
2026-01-05 15:42:01 +0800

    ios使用这些页面尝试上架

--- components/bottom-bar/bottom-bar.vue
+++ components/bottom-bar/bottom-bar.vue
@@ -69,13 +69,13 @@ export default {
         //   icon: zc,
         //   selectedIcon: zcselectimg,
         // },
-        {
-          text: "视频",
-          path: "/pages/video/video",
-          uniicon: "videocam",
-          icon: videoimg,
-          selectedIcon: videoselectimg,
-        },
+        // {
+        //   text: "视频",
+        //   path: "/pages/video/video",
+        //   uniicon: "videocam",
+        //   icon: videoimg,
+        //   selectedIcon: videoselectimg,
+        // },
         {
           text: "彩友圈",
           path: "/pages/forum/forum",
@@ -104,7 +104,7 @@ export default {
     },
   },
   created() {
-    // uni.hideTabBar();
+    uni.hideTabBar();
   },
 };
 </script>
