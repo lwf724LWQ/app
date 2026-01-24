c123e24f463982a46b62e7d4d71abd8a3a5f64a5
Lmigua <1768089277@qq.com>
2026-01-24 17:40:22 +0800

    去除无用debugger

--- pages/forum/forum.vue
+++ pages/forum/forum.vue
@@ -514,7 +514,6 @@ function onRefresh() {
     total: 0,
     maxPage: 1,
   };
-  debugger;
   loadLotteryData(currentLotteryType.value.code);
 }
 
@@ -580,9 +579,9 @@ const loadLotteryDataByType = async (lotteryType) => {
   }
   try {
     isLoadingLottery.value = true;
-    uni.showLoading({ title: "加载中..." });
+    // uni.showLoading({ title: "加载中..." });
     const response = await apiGetIssueNo({ tname: lotteryType.name });
-    uni.hideLoading();
+    // uni.hideLoading();
 
     if (response.code === 200 && response.data !== null && response.data !== undefined) {
       let issueNumber = null;
@@ -1163,9 +1162,11 @@ const loadPredictPosts = async () => {
     predictList.value = [];
   } finally {
   }
-  isLoadingPosts.value = false;
-  currentQueryKey.value = null;
-  refreshing.value = false;
+  setTimeout(() => {
+    isLoadingPosts.value = false;
+    currentQueryKey.value = null;
+    refreshing.value = false;
+  }, 500);
 };
 
 // 加载分页
